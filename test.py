ed7d2cf4616534584e16bd5969de8ee6cffcf3eb
Grant King <grantaking@gmail.com>
2025-02-13 15:20:22 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+# %%
+import pandas
+import pickle
+
+#example_df = pickle.load(open("data/pidexample_data-Tomorrow-Happiness_Train.p","rb"))
+CHEEARS_df = pickle.load(open("data/pidall_dataset_pred-_Train.p","rb"))
+# %%
